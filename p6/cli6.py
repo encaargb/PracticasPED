3376664d74396d6732c1cab03b37a20b8677fce0
Lucía Sorní Scaletti <lucia.sornisacaletti@usp.ceu.es>
2025-05-05 20:25:19 +0200

    p6 pruebas

---
+++ p6/cli6.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import socket
+import setproctitle
+
+setproctitle.setproctitle("cli6")
+
+IP = "localhost"
+PUERTO = 11111
+
+# Crear socket TCP
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    cliente.connect((IP, PUERTO))
+
+    ruta = input("Introduce la ruta del fichero: ")
+    cliente.sendall(ruta.encode())
+
+    respuesta = cliente.recv(4096).decode()
+    print("Contenido recibido:\n", respuesta)
+
+except Exception as e:
+    print(f"[cli6] Error de conexión o recepción: {e}")
+
+finally:
+    cliente.close()
